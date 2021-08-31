1fb2a31fdaf2aa6602fe14ff884d2387d742fe62
swatis9 <sdsalunke9@gmail.com>
2021-08-31 23:08:13 +0530

    Project upload

---
+++ src/test/Class1.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Class1 {
+
+	public static void main(String[] a) {
+		System.out.println("Hi");
+	}
+}
---
+++ src/test/Class2.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Class2 {
+	public static void main(String[] a) {
+		System.out.println("Hello");
+	}
+
+}
